d8ad276c4bfc81a61628f8fb40dffa63b0087e57
Blain Crawford <blaincrawford@gmail.com>
2022-02-14 17:10:30 -0500

    starting work on basic package logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import './styles.css';
+import { compareAsc, format } from 'date-fns';
+
+format(new Date(2014, 1, 11), 'yyy-MM-dd');
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
 
 let h1 = document.createElement('h1');
 h1.innerText = 'Hello, World!';
 document.body.appendChild(h1);
+console.log(dates);
+
+const printDates = (() => {
+  dates.forEach((date) => {
+    let h2 = document.createElement('h2');
+    let dateDiv = document.createElement('div');
+    h2.innerText = date;
+    dateDiv.appendChild(h2);
+    document.body.appendChild(dateDiv);
+  })
+})();
+
+printDates();
