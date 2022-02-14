4b3c9610a879e6a02b6006e394979fdc7350414c
Blain Crawford <blaincrawford@gmail.com>
2022-02-14 16:24:52 -0500

    setting up package json

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: "source-map",
+  devServer: {
+    hot: 'only',
+    static: {
+      directory: 'dist',
+      watch: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+};
