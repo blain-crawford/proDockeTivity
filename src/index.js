feef41a8a2fa92fa7e5469cd4161ee5d1eb582e4
Blain Crawford <blaincrawford@gmail.com>
2022-02-15 10:59:42 -0500

    about to run some sessionStorage tests

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ const dates = [
   new Date(1989, 6, 10),
 ];
 
-let h1 = document.createElement('h1');
-h1.innerText = 'Hello, World!';
-document.body.appendChild(h1);
-console.log(dates);
-
 const printDates = (() => {
   dates.forEach((date) => {
     let h2 = document.createElement('h2');
