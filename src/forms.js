264c0a9b3f30fb75986eb0f75902cfe22846f4ae
Blain Crawford <blaincrawford@gmail.com>
2022-02-18 14:02:13 -0500

    still trying on logic for todo creation in local storage

---
+++ src/forms.js
@@ -0,0 +1,100 @@
+class ToDoItem {
+  constructor(description, notes, deadLine, priority, project) {
+    this.description = description;
+    this.notes = notes;
+    this.deadLine = deadLine;
+    this.priority = priority;
+    // this.project = project.id
+  }
+};
+
+// Storage.prototype.setObjs = function(key, toDoArray) {
+//   for (let i = 0; i < toDoArray.length; i++) {
+//     this.setItem(`${key}${i}`, JSON.stringify(toDoArray[i]));
+//   }
+// };
+
+// Storage.prototype.getObjs = function(key) {
+//   return JSON.parse(this.getItem(key));
+// }
+
+function storageAvailable (type) {
+  let storage;
+  try {
+    storage = window[type];
+    let x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  }
+  catch (e) {
+    return e instanceof DOMException && (
+      e.code === 22 ||
+      e.code === 1014 ||
+      e.name === 'QuotaExceededError' ||
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') 
+      (storage && storage.length !== 0)
+  }
+};
+
+const toDoList = [];
+
+const toDoForm = (() => {
+  const pageBody = document.querySelector('#page-body');
+  const pageHeader = document.querySelector('#page-header'); 
+  const formClosingButton = document.querySelector("#closing-button");
+  const addToDoButton = document.querySelector('#add-todo');
+  const toDoForm = document.querySelector('#todo-form');
+  const titleInput = document.querySelector("#title-input")
+  const notesInput = document.querySelector("#notes-input")
+  const deadLineInput = document.querySelector("#deadline-input")
+  const priorityInput = document.querySelector('#priority-input')
+  const toDoAddButton = document.getElementById('add');
+  
+  const closeForm = () => {
+    toDoForm.classList.add('invisible');
+    pageBody.classList.remove('tint');
+    pageHeader.classList.remove('tint');
+  };
+
+  const openTodoForm = () => {
+    toDoForm.classList.remove('invisible')
+    pageBody.classList.add('tint');
+    pageHeader.classList.add('tint');
+  }
+
+  const clearToDoForm = () => {
+    titleInput.value = '';
+    notesInput.value = '';
+    deadLineInput.value = 'MM-dd-yyyy';
+    priorityInput.value = 'Please Choose One';
+  }
+
+  const createToDoItem = function () {
+    const toDo = new ToDoItem(titleInput.value, notesInput.value, deadLineInput.value, priorityInput.value);
+    toDoList.push(toDo);
+    clearToDoForm();
+    closeForm();
+
+    if(storageAvailable('localStorage')) {
+      if(localStorage.getItem('toDoList')) {
+        localStorage.removeItem('toDoList');
+        localStorage.setItem(`toDoList`, JSON.stringify(toDoList))
+        console.log(localStorage.length)
+      } else {
+        localStorage.setItem('toDoList', JSON.stringify(toDoList));
+      }
+    } else {
+        alert('There is no local Storage here!')
+    }
+
+  };
+
+  formClosingButton.addEventListener('click', closeForm, false);
+  addToDoButton.addEventListener('click', openTodoForm, false);
+  toDoAddButton.addEventListener('click', createToDoItem, false);
+
+  return {toDoList};
+})();
+
+export{ toDoForm, toDoList }
--- src/index.js
+++ src/index.js
@@ -1,125 +1,46 @@
 import './styles.css';
 import { compareAsc, format } from 'date-fns';
-const toDoList = [];
-const titleInput = document.querySelector("#title-input")
-const notesInput = document.querySelector("#notes-input")
-const deadLineInput = document.querySelector("#deadline-input")
-const priorityInput = document.querySelector('#priority-input')
-const addButton = document.getElementById('add');
-const pageHeader = document.querySelector('#page-header');
-const pageBody = document.querySelector('#page-body');
-const menuDropper = document.querySelector('.menu-dropper')
-const sideMenu = document.querySelector('#side-menu');
-const todoListsDisplay = document.querySelector('#todo-lists');
+import { toDoForm, toDoList } from './forms';
+
 
 /**
- * checks size of window for sideMenu placement/tent
+ * activate functionality for toDoForm
  */
- window.addEventListener('resize', () => {
-  if(window.innerWidth > 1000 && !pageBody.classList.contains('tint')) {
-    console.log(window.innerWidth);
-    todoListsDisplay.classList.remove('tint')
-    pageHeader.classList.remove('tint');
+toDoForm
+
+
+/**
+ * checks size of window for sideMenu placement/removes tent
+ */
+const handleSideMenuAndTint = (() => {
+  const pageHeader = document.querySelector('#page-header');
+  const pageBody = document.querySelector('#page-body');
+  const menuDropper = document.querySelector('.menu-dropper')
+  const sideMenu = document.querySelector('#side-menu');
+  const todoListsDisplay = document.querySelector('#todo-lists');
+
+  const removeTintForLargeScreens = (() => {
+    window.addEventListener('resize', () => {
+      if(window.innerWidth > 1000 && !pageBody.classList.contains('tint')) {
+        sideMenu.classList.remove('show-side-menu');
+        todoListsDisplay.classList.remove('tint');
+        pageHeader.classList.remove('tint');
+      }
+    })
+  })();
+
+  const showSideMenu = function () {  
+    sideMenu.classList.toggle('show-side-menu');
+    todoListsDisplay.classList.toggle('tint');
+    pageHeader.classList.toggle('tint');
   }
-});
 
-class ToDoItem {
-  constructor(description, notes, deadLine, priority) {
-    this.description = description;
-    this.notes = notes;
-    this.deadLine = deadLine;
-    this.priority = priority;
-  }
-};
-
-const createToDoItem = function () {
-  const toDo = new ToDoItem(titleInput.value, notesInput.value, deadLineInput.value, priorityInput.value);
-  toDoList.push(toDo);
-  console.log(toDoList);
-};
-
-addButton.addEventListener('click', createToDoItem, false);
-
-const hideForm = (() => {
-  const formClosingButton = document.querySelector("#closing-button");
-  const toDoForm = document.querySelector('#todo-form')
-  
-  const closeForm = () => {
-    toDoForm.classList.add('invisible');
-    pageBody.classList.remove('tint');
-    pageHeader.classList.remove('tint');
-  };
-
-  formClosingButton.addEventListener('click', closeForm, false);
+  menuDropper.addEventListener('click', showSideMenu, false);
 })();
 
-const showSideMenu = () => {
-
-  sideMenu.classList.toggle('show-side-menu');
-  todoListsDisplay.classList.toggle('tint')
-  pageHeader.classList.toggle('tint');
-};
-
-menuDropper.addEventListener('click', showSideMenu, false);
 
 
 
 
-// const printDates = (() => {
-//   dates.forEach((date) => {
-//     let h2 = document.createElement('h2');
-//     let dateDiv = document.createElement('div');
-//     h2.innerText = date;
-//     const checkOff = document.createElement('input');
-//     checkOff.type = 'checkbox'
-//     dateDiv.appendChild(checkOff);
-//     dateDiv.style.cssText = 'display: flex;'
-//     dateDiv.appendChild(h2);
-//     document.body.appendChild(dateDiv);
-    
-//   })
-// })();
-
-// function storageAvailable (type) {
-//   let storage;
-//   try {
-//     storage = window[type];
-//     let x = '__storage_test__';
-//     storage.setItem(x, x);
-//     storage.removeItem(x);
-//     return true;
-//   }
-//   catch (e) {
-//     return e instanceof DOMException && (
-//       e.code === 22 ||
-//       e.code === 1014 ||
-//       e.name === 'QuotaExceededError' ||
-//       e.name === 'NS_ERROR_DOM_QUOTA_REACHED') 
-//       (storage && storage.length !== 0)
-//   }
-// };
-
-// const colorSelector = document.createElement('input');
-// colorSelector.type = 'color'
-// document.body.append(colorSelector);
-// const h2 = document.querySelectorAll('h2');
-// h2.forEach((heading) => {
-//   heading.style.cssText = `color: ${localStorage['backgroundColor']};`
-// })
-// colorSelector.addEventListener('change', function () {
-//   console.log(this.value);
-//   localStorage.setItem('backgroundColor', this.value);
-//   console.log(localStorage);
-//   if(storageAvailable('localStorage')) {
-//     const h2 = document.querySelectorAll('h2')
-//     h2.forEach((heading) => {
-//       heading.style.cssText = `color: ${localStorage['backgroundColor']};`
-//     })
-//   }
-// })
-
-// colorSelector.value = localStorage['backgroundColor'];
 
 
-
-// console.log(localStorage);
